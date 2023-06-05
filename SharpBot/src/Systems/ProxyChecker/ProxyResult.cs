d99401960b9c05ef0628b3ee18e5a420bf34d13e
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-06-05 01:17:04 -0300

    submit .idea, and extra, untracked source code file.

---
+++ SharpBot/src/Systems/ProxyChecker/ProxyResult.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpBot.Systems.ProxyChecker;
+
+public struct ProxyResult {
+    public string host;
+    public ushort port;
+    public bool isSuccess;
+
+    public ProxyResult(string host, ushort port, bool isSuccess) {
+        this.host = host;
+        this.port = port;
+        this.isSuccess = isSuccess;
+    }
+
+    public ProxyResult(string host, int port, bool isSuccess) : this(host, (ushort)port, isSuccess) { // Call ctor.
+        if (port > ushort.MaxValue)
+            throw new ArgumentOutOfRangeException(nameof(port), "Port must be less than 65535!");
+    }
+}
